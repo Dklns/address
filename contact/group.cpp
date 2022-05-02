474fc7bafb716e51d13a3d2c0fbf610951eda6c9
Dklns <d.klns.245@gmail.com>
2022-05-02 17:22:23 +0800

    弥补对查找名字拼音那段代码的理解错误

--- contact/group.cpp
+++ contact/group.cpp
@@ -11,7 +11,20 @@ void group::addContact(contact* contact, std::map<std::string, std::string>& map
 	char ch = contact->m_name[0];
 	// 判断联系人名字是中文还是英文
 	if (ch <= 'z' && ch >= 'a' || ch <= 'Z' && ch >= 'A') contact->m_pinyin = contact->m_name;
-	else contact->m_pinyin = map[contact->m_name];
+	else {
+		std::string pinyin;
+		std::string word;
+		// 获取名字的总拼音，因为字典里只记录了每个字的拼音，所以需要查名字中每个字的拼音
+		for (int i = 0; i < contact->m_name.size(); i += 2)
+		{
+			// 常用汉字是以2字节储存的
+			word = contact->m_name[i];
+			word += contact->m_name[i + 1];
+			pinyin += map[word];
+		}
+		pinyin.push_back('\0');
+		contact->m_pinyin = pinyin;
+	}
 	 
 	if (m_group.size() == 0) m_group.push_back(contact);
 	else {
